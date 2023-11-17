5ee20173395168c16ac46225f50337d4458a338a
Brian Scherner <brianscherner@gmail.com>
2023-11-17 13:08:13 -0800

    Update DB, add create functionality for Engineer.

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -22,5 +22,22 @@ namespace Factory.Controllers
       List<Engineer> model = _db.Engineers.ToList();
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(engineer);
+      }
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -7,10 +7,13 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
-    // public int MachineId { get; set; }
+
+    [Required(ErrorMessage = "The engineer's name can't be empty!")]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "The engineer's description can't be empty!")]
     public string Description { get; set; }
     // public Machine Machine { get; set; }
-    // public List<Machine> Machines { get; set; }
+    public List<Machine> Machines { get; set; }
   }
 }
