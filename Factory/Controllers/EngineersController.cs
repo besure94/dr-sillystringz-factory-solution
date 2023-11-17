5b901d29c76aef6c6dece3698d0fd8b4bcbef634
Brian Scherner <brianscherner@gmail.com>
2023-11-17 13:33:30 -0800

    Add functionality to view Engineer details.

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -39,5 +39,12 @@ namespace Factory.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Details(int id)
+    {
+      Engineer thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisEngineer);
+    }
+
   }
 }
