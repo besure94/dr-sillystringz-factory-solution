4589971e6ce019d06d73e5597d5ab9cbe796b3fb
Brian Scherner <brianscherner@gmail.com>
2023-11-17 13:22:38 -0800

    Add functionality to create a Machine.

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -7,7 +7,11 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
+
+    [Required(ErrorMessage = "The machine's name can't be empty!")]
     public string Name { get; set; }
+
+    [Required(ErrorMessage = "The machine's description can't be empty!")]
     public string Description { get; set; }
     public List<Engineer> Engineers { get; set; }
 
