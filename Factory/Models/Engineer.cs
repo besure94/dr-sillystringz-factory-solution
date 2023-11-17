7efc207e785e2966b0baeb0908391e26cf5752ed
Brian Scherner <brianscherner@gmail.com>
2023-11-17 10:00:45 -0800

    Initial commit.

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public int EngineerId { get; set; }
+    // public int MachineId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    // public Machine Machine { get; set; }
+    public List<Machine> Machines { get; set; }
+  }
+}
---
+++ Factory/Models/Machine.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Factory.Models
+{
+  public class Machine
+  {
+    public int MachineId { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Engineer> Engineers { get; set; }
+
+  }
+}
