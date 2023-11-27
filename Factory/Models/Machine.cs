a3c1b2b212ac47e08ce316ed70453e32ddad8992
Brian Scherner <brianscherner@gmail.com>
2023-11-27 14:33:40 -0800

    Edit regex model validation for both Models.

--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -9,7 +9,7 @@ namespace Factory.Models
     public int MachineId { get; set; }
 
     [Required(ErrorMessage = "The Name field is required."),MaxLength(30)]
-    [RegularExpression("^[A-Za-z-'0-9 ]+$", ErrorMessage = "The Name field contains invalid characters. You may enter only letters, numbers, spaces, and the characters ',' or '-'.")]
+    [RegularExpression("^[A-Za-z-'0-9 ]+$", ErrorMessage = "The Name field contains invalid characters. You may enter only letters, numbers, spaces, apostrophes ('), and hyphens (-).")]
     public string Name { get; set; }
 
     [Required(ErrorMessage = "The Description field is required."), MaxLength(250)]
